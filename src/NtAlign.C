1220e82a7a10685965b31699e671830f80689990
Koen Deforche <koen@emweb.be>
2010-12-03 15:27:45 +0100

    optimize using fragment begin

--- src/NtAlign.C
+++ src/NtAlign.C
@@ -5,6 +5,7 @@
 
 #include "Utils.h"
 
+#include <boost/algorithm/string.hpp>
 #include <set>
 #include <vector>
 #include <iostream>
@@ -18,13 +19,26 @@ NtAlign::NtAlign()
 
 void writeSequenceWithoutInsertions(std::ostream& o,
 				    const seq::NTSequence& reference,
-				    const seq::NTSequence& target)
+				    const seq::NTSequence& target,
+				    int begin,
+				    int size)
 {
   o << ">" << target.name() << " " << target.description() << std::endl;
 
+  int j = 0;
+  for (unsigned i = 0; i < begin; ++i) {
+    o << "-";
+    ++j;
+  }
+
   for (unsigned i = 0; i < reference.size(); ++i)
-    if (reference[i] != seq::Nucleotide::GAP)
+    if (reference[i] != seq::Nucleotide::GAP) {
       o << target[i];
+      ++j;
+    }
+
+  for (unsigned i = 0; i < (size - j); ++i)
+    o << "-";
 
   o << std::endl;
 }
@@ -54,10 +68,30 @@ void NtAlign::execute(std::map<std::string, std::string> & parameters)
       seq::NTSequence s;
       target_f >> s;
       if (target_f) {
-	seq::NTSequence r = reference;
+	int begin = 0;
+	int end = reference.size() - 1;
+
+	std::string d = s.description();
+	boost::trim(d);
+	if (!d.empty()) {
+	  std::vector<std::string> values;
+	  boost::split(values, d, boost::is_any_of("-"));
+
+	  if (values.size() == 2) {
+	    const int MARGIN = 50;
+	    begin = std::max(begin,
+			     atoi(values[0].substr(1).c_str()) - MARGIN);
+	    end = std::min(end,
+			   atoi(values[1].substr(0, values[1].length() - 1)
+				.c_str()) + MARGIN);
+	  }
+	}
+
+	seq::NTSequence r(reference.begin() + begin, reference.begin() + end);
+
 	double score = seq::Align(r, s);
 	if (score >= cutoff)
-	  writeSequenceWithoutInsertions(output_f, r, s);
+	  writeSequenceWithoutInsertions(output_f, r, s, begin, reference.size());
       }
     }
   } catch(seq::ParseException& e) {
